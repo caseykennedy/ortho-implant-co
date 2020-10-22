907e48f92deaba0ae9f3e299219b83db22c29fdc
Casey K <tehcnicolour@gmail.com>
2020-10-22 13:06:06 -0700

    updates sanity adds deploy plugin

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,27 @@
+export default {
+  widgets: [
+    {
+      name: "sanity-tutorials"
+    },
+    {
+      name: "project-info"
+    },
+    {
+      name: "project-users"
+    },
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Ortho Implant Co. Website",
+            apiId: "d3cf9eda-d5b1-4eb2-a6dd-6750ca89e81c",
+            buildHookId: "5f91dd8a5121d30fdbc20e6f",
+            name: "oic"
+          }
+        ]
+      }
+    }
+  ]
+};
